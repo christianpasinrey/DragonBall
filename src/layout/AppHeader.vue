69a049d19c0c3b63c33e5ff37ad238d34857515c
Christian Pasín Rey <christian.pasin.rey@gmail.com>
2024-08-16 15:20:21 +0200

    Tablet/mobile menu

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -6,7 +6,7 @@ const global = useGlobalStore();
 
 </script>
 <template>
-    <header class="flex w-full items-center content-center align-middle px-6 py-3 sticky top-0 left-0 right-0 z-[999]">
+    <header class="flex w-full items-center content-center align-middle justify-between px-6 py-3 sticky top-0 left-0 right-0 z-[999]">
         <div class="logo-wrapper">
             <img 
                 alt="Dragon ball logo" 
@@ -26,18 +26,20 @@ const global = useGlobalStore();
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
             </svg>
         </button>
-
-        <nav v-if="global.showingMenu" class="md:hidden flex flex-col justify-evenly gap-2 text-center w-screen h-screen bg-[rgb(255,255,255,0.7)] absolute inset-0">
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/characters">Characters</RouterLink>
-            <RouterLink to="/planets">Planets</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <button @click.prevent="global.toggleMenu">
-                <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                </svg>
-            </button>
-        </nav>
+        <transition name="fade">
+          <nav v-if="global.showingMenu" class="md:hidden absolute inset-0 flex flex-col justify-evenly gap-2 text-center w-screen h-screen bg-[rgb(255,255,255,0.7)] transition-all ease-in-out duration-300">
+              <RouterLink to="/">Home</RouterLink>
+              <RouterLink to="/characters">Characters</RouterLink>
+              <RouterLink to="/planets">Planets</RouterLink>
+              <RouterLink to="/about">About</RouterLink>
+              <button @click.prevent="global.toggleMenu" class="text-black absolute top-6 right-6">
+                  <svg class="w-8 h-8 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                  </svg>
+              </button>
+          </nav>
+        </transition>
+        
     </header>
 </template>
 <style scoped>
@@ -50,5 +52,34 @@ const global = useGlobalStore();
   header{
     background: linear-gradient(135deg, rgba(255,165,0,0.8), rgba(255,69,0,0.8));
     opacity: 0.8;
+  }
+
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-to, .fade-leave {
+    opacity: 1;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
   }
 </style>
