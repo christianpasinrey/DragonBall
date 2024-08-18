2cd034e4df8563eb8e63151ee5b8806a78ec6a58
Christian Pasín Rey <christian.pasin.rey@gmail.com>
2024-08-18 23:28:46 +0200

    dark theme

--- src/layout/AppBody.vue
+++ src/layout/AppBody.vue
@@ -11,6 +11,6 @@
         background-color: rgba(255, 255, 255, 0.7);
     }
     html.dark div{
-        background-color: rgba(0, 0, 0, 0.7);
+        background-color: rgba(0, 0, 0, 0.8);
     }
 </style>
